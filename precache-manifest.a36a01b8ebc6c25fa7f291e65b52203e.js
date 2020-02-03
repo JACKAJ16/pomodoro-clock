7af92ee101f38efae2584942d2113673be91f133
JACK <zhenya271@gmail.com>
2020-02-03 23:16:59 +0200

    Updates

---
+++ precache-manifest.a36a01b8ebc6c25fa7f291e65b52203e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb0cce462dd12a1da9c18d326995704",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "683c45f19d5589c8c4d8",
+    "url": "/pomodoro-clock/static/css/main.c9c14bdf.chunk.css"
+  },
+  {
+    "revision": "aa0f609c7abf6e30fb26",
+    "url": "/pomodoro-clock/static/js/2.16bd0a08.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro-clock/static/js/2.16bd0a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683c45f19d5589c8c4d8",
+    "url": "/pomodoro-clock/static/js/main.8ccf2b38.chunk.js"
+  },
+  {
+    "revision": "651b19b1a96a4e4082ca",
+    "url": "/pomodoro-clock/static/js/runtime-main.e6032bdd.js"
+  }
+]);
